33b46ea80564dd00dbb6bb0bbaab823abbd45b7d
gotuiyan <gto.rikujyo@icloud.com>
2022-12-25 19:48:35 +0900

    added max_len option to dataset.py

--- dataset.py
+++ dataset.py
@@ -5,13 +5,25 @@ from typing import List, Tuple
 from transformers import PreTrainedTokenizer
 
 class Dataset():
-    def __init__(self, srcs: List[str], tokenizer: PreTrainedTokenizer) -> None:
+    def __init__(
+        self,
+        srcs: List[str],
+        tokenizer: PreTrainedTokenizer,
+        max_len: int
+    ) -> None:
         self.srcs = srcs
         self.tokenizer = tokenizer
+        self.max_len = max_len
     
     def __getitem__(self, idx: int) -> dict:
         src = self.srcs[idx]
-        encode = self.tokenizer.batch_encode_plus([src], max_length=128, padding="max_length", truncation=True, return_tensors='pt')
+        encode = self.tokenizer.batch_encode_plus(
+            [src],
+            max_length=self.max_len,
+            padding="max_length",
+            truncation=True,
+            return_tensors='pt'
+        )
         return {
             'input_ids': encode['input_ids'].squeeze(),
             'attention_mask': encode['attention_mask'].squeeze()
@@ -22,15 +34,15 @@ class Dataset():
 
 def generate_dataset(
     input_file: str,
-    tokenizer: PreTrainedTokenizer
+    tokenizer: PreTrainedTokenizer,
+    max_len: int
 ) -> Dataset:
     '''
-    This function recieves input file path(s) and returns Dataset instance.
+    This function recieves input file path(s) and returns a Dataset instance.
     '''
     srcs = open(input_file).read().rstrip().split('\n')
     return Dataset(
         srcs=srcs,
-        tokenizer=tokenizer
+        tokenizer=tokenizer,
+        max_len=max_len
     )
-
-    
